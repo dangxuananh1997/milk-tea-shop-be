84d82fabe1a6708795bf46543a6e2ec8a1d031f9
Duong <duongphse62746@fpt.edu.vn>
2018-07-04 10:52:52 +0700

    admin api fix ninject

---
+++ MilkTeaShop/API.MilkteaAdmin/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Core.AppService.Business;
+using Core.ObjectModel.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.MilkteaAdmin.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IHttpActionResult GetAllProduct()
+        {
+            List<Product> products = _productService.GetAllProduct().ToList();
+
+            return Ok(products);
+        }
+    }
+}
