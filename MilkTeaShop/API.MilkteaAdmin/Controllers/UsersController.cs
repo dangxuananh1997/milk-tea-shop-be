084e98125f72144c3b42d2ace90e43cc0643d826
Duong <duongphse62746@fpt.edu.vn>
2018-07-09 15:22:24 +0700

    add user management action

--- MilkTeaShop/API.MilkteaAdmin/Controllers/UsersController.cs
+++ MilkTeaShop/API.MilkteaAdmin/Controllers/UsersController.cs
@@ -1,13 +1,104 @@
-﻿using System;
+﻿using API.MilkteaAdmin.Models;
+using Core.AppService.Business;
+using Core.ObjectModel.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace API.MilkteaAdmin.Controllers
 {
     public class UsersController : ApiController
     {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            this._userService = userService;
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                List<UserVM> result = AutoMapper.Mapper.Map<List<User>, List<UserVM>>
+                    (_userService.GetAllUser().ToList());
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                UserVM result = AutoMapper.Mapper.Map<User, UserVM>
+                    (_userService.GetUser(id));
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult Create(UserCM cm)
+        {
+            try
+            {
+                User model = AutoMapper.Mapper.Map<UserCM, User>(cm);
+                _userService.CreateUser(model);
+                _userService.SaveUserChanges();
+
+                UserVM result = AutoMapper.Mapper.Map<User, UserVM>(model);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update(UserUM um)
+        {
+            try
+            {
+                User model = AutoMapper.Mapper.Map<UserUM, User>(um);
+                _userService.UpdateUser(model);
+                _userService.SaveUserChanges();
+
+                UserVM result = AutoMapper.Mapper.Map<User, UserVM>(model);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                _userService.DeleteUser(id);
+                _userService.SaveUserChanges();
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
     }
 }
